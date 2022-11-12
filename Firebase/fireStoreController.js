acd4aaf2b3f8aac347663405b57f0014846bf4fe
perezxalan <perezxalan@gmail.com>
2022-11-11 22:13:58 -0600

    created the workout function that creates a subcollections of workouts within the user

--- Firebase/fireStoreController.js
+++ Firebase/fireStoreController.js
@@ -17,8 +17,8 @@ export async function fetchObject(uid){
         const docRef = await doc(db, "users" ,uid);
         
         const docSnapShot = await getDoc(docRef);
-        console.log(docRef);
-        console.log(docSnapShot.data());
+        console.log('clg docref in fetchobj ',docRef);
+        console.log('clg docsnap: ', docSnapShot.data());
         return docSnapShot.data();
     }catch(e){
         
@@ -39,7 +39,7 @@ export const createObject = async (email,age,weight,heightFt, heightIn,uid) =>{
             heightIn: heightIn
         };
         const docRef = await setDoc(collectionRef,object)
-        console.log(object);
+        console.log('clg create obj func: ', object);
         return object;
     }catch(e){
     
@@ -47,6 +47,8 @@ export const createObject = async (email,age,weight,heightFt, heightIn,uid) =>{
         return e;
     }
     
+    
+
 
     // return ({
     //     email: email,
@@ -58,6 +60,51 @@ export const createObject = async (email,age,weight,heightFt, heightIn,uid) =>{
     
 }
 
+/*
+*   This function will create a new workout object in the database, referencing user uid
+*   @param {string} name - the name of the workout
+*   @param {string} description - the description of the workout
+*   @param {string} uid - the user id of the user who created the workout
+*   @param {string} date - the date the workout was created
+*   @param {string} time - the time the workout was created
+*   @param {string} workoutType - the type of workout (cardio, strength, etc.)
+*   @param {string} workoutDuration - the duration of the workout
+*   @param {string} workoutNotes - any notes the user wants to add to the workout
+*   @param {string} workoutSets - the number of sets in the workout
+*   @param {string} workoutReps - the number of reps in the workout
+*   @param {string} workoutWeight - the weight used in the workout
+*   @return {object} - the workout object
+*/
+
+
+export const createWorkout = async (workoutName,workoutType,workoutDate,workoutTime,workoutDuration,workoutNotes, workoutSets, workoutReps, workoutWeights) => 
+{
+    // const userRef = doc(db,"users",uid );
+    const docRef = doc(db, "users", email);
+    // const docRef = db.collection('users').doc(email).collection('workouts').doc(workoutName);
+    const colRef = collection(docRef, "workouts")
+    // const docRef = db.collection('users').doc(email).collection('workouts').doc(workoutName);
+    try {
+        const workoutObj = {
+            workoutName: workoutName,
+            workoutType: workoutType,
+            workoutDate: workoutDate,
+            workoutTime: workoutTime,
+            workoutDuration: workoutDuration,
+            workoutNotes: workoutNotes,
+            workoutSets: workoutSets,
+            workoutReps: workoutReps,
+            workoutWeights: workoutWeights  
+        };
+        const workoutref = await setDoc(colRef,workoutObj)
+        // console.log('clg obj', workoutObj);
+        // console.log('clg user ref: ', docRef);
+        return workoutObj;
+    } catch (error) {
+        console.log('Unable to create a workout due to: ',error);
+    }
+
+}
 //module.exports = {authenticate,createObject};
 
 
