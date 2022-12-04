fae2b113ceff04514bbe96bde6c42ac8f96c3e01
perezxalan <perezxalan@gmail.com>
2022-12-04 17:28:56 -0600

    cleaned up app.js file

--- App.js
+++ App.js
@@ -1,27 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
-import { testing } from './Firebase/firebase.js';
-import { logIn, register } from './Firebase/firebaseAuth.js';
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import GestureRecognizer, {swipeDirections} from 'react-native-swipe-gestures';
+import Home from './src/components/Home';
+import Login from './src/components/Login';
+import Register from './src/components/Register';
+//npm i -S react-native-swipe-gesturesimport Home from "./Home";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{headerShown: false}}>
+                <Stack.Screen name="Home" component={Home} />
+                <Stack.Screen name="Login" component={Login} />
+                <Stack.Screen name="Register" component={Register} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+
 }
 
 export default App;
